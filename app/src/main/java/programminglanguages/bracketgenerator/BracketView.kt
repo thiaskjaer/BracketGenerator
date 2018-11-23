e035416300ea8b0c533293655d539147b234933f
Thias Kjaer <s.thias@gmail.com>
2018-11-23 15:55:37 -0700

    Added the BracketView Activity

---
+++ app/src/main/java/programminglanguages/bracketgenerator/BracketView.kt
@@ -0,0 +1,12 @@
+package programminglanguages.bracketgenerator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BracketView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bracket_view)
+    }
+}
